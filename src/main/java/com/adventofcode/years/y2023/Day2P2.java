3ea438abca8430d52303716a7c04d37d5c9e8c3e
eduardo <eduardovanbenthum@gmail.com>
2024-11-16 14:29:20 +0100

    made y2 d2 p2. added y2 d3 p1

--- src/main/java/com/adventofcode/years/y2023/Day2P2.java
+++ src/main/java/com/adventofcode/years/y2023/Day2P2.java
@@ -12,6 +12,37 @@ public class Day2P2 extends IDay {
 
     @Override
     protected String logic() {
-        return "";
+        int result = 0;
+
+        for (String game : input) {
+
+            String[] rounds = game.split(":")[1].split(";");
+
+            int blue = 0;
+            int red = 0;
+            int green = 0;
+
+            for (String round : rounds) {
+
+                String[] cubes = round.split(",");
+
+                for (String cube : cubes) {
+                    String color = cube.split(" ")[2];
+                    int cubeNumber = Integer.parseInt(cube.split(" ")[1]);
+                    if ("green".equals(color)) {
+                        green = Math.max(green, cubeNumber);
+                    }
+                    else if ("blue".equals(color)) {
+                        blue = Math.max(blue, cubeNumber);
+                    }
+                    else if ("red".equals(color)) {
+                        red = Math.max(red, cubeNumber);
+                    }
+                }
+            }
+
+            result += blue * red * green;
+        }
+        return result + "";
     }
 }
