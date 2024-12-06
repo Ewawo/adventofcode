01b5a3cbedaf05c62423919197e8fe89c2b23ebe
eduardo <eduardovanbenthum@gmail.com>
2024-12-06 07:18:05 +0100

    setup day 6

---
+++ src/main/java/com/adventofcode/years/y2024/Y24D6P1.java
@@ -0,0 +1,17 @@
+package com.adventofcode.years.y2024;
+
+import com.adventofcode.years.Day;
+
+public class Y24D6P1 extends Day {
+
+    public Y24D6P1() {
+        super(2024, 6, 1);
+        String path = "y2024/day6.txt";
+        super.readInput(path);
+    }
+
+    @Override
+    protected int logic() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/adventofcode/years/y2024/Y24D6P2.java
@@ -0,0 +1,17 @@
+package com.adventofcode.years.y2024;
+
+import com.adventofcode.years.Day;
+
+public class Y24D6P2 extends Day {
+
+    public Y24D6P2() {
+        super(2024, 6, 2);
+        String path = "y2024/day6.txt";
+        super.readInput(path);
+    }
+
+    @Override
+    protected int logic() {
+        return 0;
+    }
+}
